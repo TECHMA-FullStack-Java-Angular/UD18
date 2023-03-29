1fe00ceefc30a1834eaaa418cc2b3a20b6dbbb63
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-03-29 13:48:01 +0200

    Creacion de base de datos con nombre propio

--- src/db/dbConection.java
+++ src/db/dbConection.java
@@ -1,4 +1,5 @@
 package db;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,14 +13,15 @@ public class dbConection {
 
     public static void main(String[] args) {
         dbConection dbc = new dbConection();
-        dbc.connect("192.168.1.128:3306");
+        dbc.connect("192.168.1.128:3306","remote","por.java12DABA");
+        dbc.createDB("Pedro_pintame");
         dbc.closeConnection();
     }
 
-    public void connect(String IP) {
+    public void connect(String IP,String user,String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://"+IP+" ", "remote", "por.java12DABA");
+            conexion = DriverManager.getConnection("jdbc:mysql://"+IP+" ",user ,password );
             System.out.print("Server Connected");
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.print("No se ha podido conectar con mi base de datos");
@@ -35,6 +37,23 @@ public class dbConection {
         } catch (SQLException ex) {
             Logger.getLogger(dbConection.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    public void createDB(String dbName)
+    {
+    	
+    	try
+    	{
+    		String kuery="CREATE DATABASE "+dbName+" ;";
+    		Statement st=conexion.createStatement();
+    		System.out.println(kuery);
+    		st.executeUpdate(kuery);
+    		System.out.println("Ey que llego");
+    		JOptionPane.showMessageDialog(null,"Se ha creado la base de datos"+dbName+" correctamente");
+    	}
+    	catch(SQLException ex)
+    	{
+    		Logger.getLogger(dbConection.class.getName()).log(Level.SEVERE,null,ex);
+    	}
     }
 }
 
