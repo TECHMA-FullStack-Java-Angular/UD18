cccc7d2aecb7b4754892f79784fb90a0618e5c43
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-03-29 11:10:29 +0200

    Conection for test

--- src/db/dbConection.java
+++ src/db/dbConection.java
@@ -3,34 +3,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
+
 
 public class dbConection {
-	public static void main(String[] args) 
+	public static Connection conectorDeDB()
 	{
 		try 
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conexion= DriverManager.getConnection("jdbc:mysql://localhost:3306?userTimezone=true&serverTimezone=UTC","remote","por.java12DABA");
+			Connection conexion= DriverManager.getConnection("jdbc:mysql://192.168.4.66:3306","remote","P@ssw0rd_Remote");
 			System.out.print("Server Connected");
+			return conexion;
 			
 		}catch(SQLException | ClassNotFoundException ex)
 		{
 			System.out.print("No se ha podido conectar con mi base de datos");
 			System.out.print(ex);
 		}
+		return null;
 	}
-	public void closeConnection()
+	public static void closeConnection(Connection conection)
 	{
 		try 
 		{
-			conexion.Close();
-			JOptionPane.showMessageDialog(null, "Se ha finalizado la conexion con el servidor");
-			
-		}catch(SQLException ex)
+			conection.close();
+			System.out.println("Connection ended");
+		}
+		catch(SQLException ex)
 		{
-			Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE,null,ex);
+			System.out.println("Connection error on close");
+			System.out.println(ex);
 		}
 	}
+	
 
 }
